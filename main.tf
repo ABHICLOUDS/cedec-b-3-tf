42177ab4467ecf274c919271d7776a9c50037688
abhi-prasad <abhiprasad@mail>
2023-05-13 21:33:11 +0530

    commit15

--- main.tf
+++ main.tf
@@ -4,13 +4,21 @@ provider "aws" {
 
 resource "aws_instance" "my-inst" {
   ami           = "ami-08333bccc35d71140"
-
   instance_type = "t2.micro"
   associate_public_ip_address = true
   key_name ="ohio-new"
   security_groups = ["default"]
   vpc_security_group_ids = [aws_security_group.this.id]
   iam_instance_profile ="S3-fullacces-role"
+  user_data = <<EOF
+  #!/bin/bash
+sudo yum update -y
+sudo yum install httpd -y
+sudo touch /var/www/html/index.html
+sudo echo "Welcome to my website $(hostname)" > /var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd 
+EOF
 
    root_block_device{
       volume_size = 10
