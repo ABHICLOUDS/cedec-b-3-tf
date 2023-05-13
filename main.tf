1df5da6a2581826df054acc2fbe107ec1ba48339
abhi-prasad <abhiprasad@mail>
2023-05-13 21:15:36 +0530

    commit12

--- main.tf
+++ main.tf
@@ -27,6 +27,20 @@ resource "aws_security_group" "this" {
   ingress {
     description      = "TLS from VPC"
     from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
