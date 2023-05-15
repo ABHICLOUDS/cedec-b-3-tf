2a96665c6ae9a5ab4867e315f1de79599cd94f7b
abhi-prasad <abhiprasad@mail>
2023-05-15 19:55:27 +0530

    commit16

--- iam.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_user" "demo" {
-  count = "${length(var.username)}"
-  name = "${element(var.username,count.index )}"
-}
--- sg.tf
+++ sg.tf
@@ -9,9 +9,7 @@ resource "aws_security_group" "this" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
+    ingress {    # from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
