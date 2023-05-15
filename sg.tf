8f030674fb58f9f9e1a14992673942d49562be73
abhi-prasad <abhiprasad@mail>
2023-05-15 20:47:04 +0530

    commit17

--- sg.tf
+++ sg.tf
@@ -8,7 +8,8 @@ resource "aws_security_group" "this" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-    ingress {    # from_port        = 80
+    ingress {    
+    from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
